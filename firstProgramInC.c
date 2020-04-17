14fc0c3a7d9522e18e3e6f09cf65b8608a804080
Amr Zain <Amr@zains-MacBook-Pro.local>
2020-04-17 17:41:49 +0200

    My first app in c

---
+++ firstProgramInC.c
@@ -0,0 +1,11 @@
+//Preprocessor program processes the code.
+// Lines beginning with # are processed by the preprocessor before compilation
+#include <stdio.h>
+
+//function main begins program execution
+int main () {
+
+printf("Hello,World!\n\a");
+return 0 ;
+
+}// end function main
